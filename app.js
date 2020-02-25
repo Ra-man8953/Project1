56e22059f04595cb5e751c0502397e581daae869
Raman srivastava <61067338+Ra-man8953@users.noreply.github.com>
2020-02-25 07:33:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ const review = require('./controllers/reviews/review');
 module.exports = function(app) {
 
 
+
     app.use(bodyParser.text())
 
-    app.use('/api/user', user);
+    app.use('/api/auth', user);
+
     app.use('/api/auth', login);
-    app.use('/api/product', product);
+
+    const checkAuth = (req, res, next) => {
+        console.log(req.body)
+        if (!req.headers.authorization) {
+            return res.status(403).json({ error: 'No credentials sent!' });
+        }
+
+        // check valid
+        console.log(req.headers.authorization);
+
+        next();
+    };
+    app.use(checkAuth);
+
+    app.use('/api/products', product);
     app.use('/api/review', review);
 
     //invalid url
